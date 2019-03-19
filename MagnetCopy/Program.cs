43c023f2aa286be94dda80a31718063cda44bc15
motolies <knw1234@gmail.com>
2019-03-20 00:41:10 +0900

    윈폼에 찍는 부분 까지....

---
+++ MagnetCopy/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagnetCopy
+{
+    class Program
+    {
+
+        // https://ehdrn.tistory.com/295
+
+        const int WM_COPYDATA = 0x4A;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, ref COPYDATASTRUCT lParam);
+
+        public struct COPYDATASTRUCT
+        {
+            public IntPtr dwData;
+            public int cbData;
+
+            [MarshalAs(UnmanagedType.LPStr)]
+            public string lpData;
+        }
+
+        public static void SendMessage(string msg)
+        {
+            Process[] pro = Process.GetProcessesByName("MagnetCopyUI");
+            if (pro.Length > 0)
+            {
+                byte[] buff = System.Text.Encoding.Default.GetBytes(msg);
+
+                COPYDATASTRUCT cds = new COPYDATASTRUCT();
+                cds.dwData = IntPtr.Zero;
+                cds.cbData = buff.Length + 1;
+                cds.lpData = msg;
+
+                SendMessage(pro[0].MainWindowHandle, WM_COPYDATA, 0, ref cds);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+
+            try
+            {
+                string param = args[0];
+
+                Console.WriteLine(param);
+                SendMessage(param);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Environment.Exit(0);
+            }
+            
+        }
+    }
+}
---
+++ MagnetCopyUI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagnetCopyUI
+{
+    public partial class Form1 : Form
+    {
+        const int WM_COPYDATA = 0x4A;
+
+        public struct COPYDATASTRUCT
+        {
+            public IntPtr dwData;
+            public int cbData;
+
+            [MarshalAs(UnmanagedType.LPStr)]
+            public string lpData;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            try
+            {
+                switch (m.Msg)
+                {
+                    case WM_COPYDATA:
+                        COPYDATASTRUCT cds = (COPYDATASTRUCT)m.GetLParam(typeof(COPYDATASTRUCT));
+                        ReceivedDataProcess(cds.lpData);
+                        break;
+                    default:
+                        base.WndProc(ref m);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ReceivedDataProcess(string data)
+        {
+            // 데이터 받으면 처리할 부분
+            richTextBox1.AppendText(data + Environment.NewLine);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
